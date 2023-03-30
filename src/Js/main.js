a23bfdcc5db1bd163a8111daf19ce9fe863d2a75
prince0010 <princenagac12@gmail.com>
2023-03-30 18:48:12 +0800

    Vite Portfolio with pure HTML, CSS, vanillaJs

---
+++ src/Js/main.js
@@ -0,0 +1,12 @@
+import '../Css/style.css';
+import '../Css/modern_normalize.css';
+import '../Css/components/header.css';
+import '../Css/components/head.css';
+import '../Css/components/about.css';
+import '../Css/components/feature.css';
+import '../Css/components/work.css';
+import '../Css/components/contact.css';
+import '../Css/components/footer.css';
+// the utils.css must be the last one since its the most priority of them all or else this css will not work
+import '../Css/utils.css';
+
